eebbb9c2d5bf2b0ad347f25dafd4bf939bbe50aa
Jacob Gorman <violinoman18@aol.com>
2016-09-29 18:12:09 -0400

    Added AHATest
    Looks like this GitHub thing is working

---
+++ ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/AHATest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+/**
+ * Created by Jacob on 9/29/2016.
+ */
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class AHATest extends OpMode {
+    public DcMotor leftMotor;
+    public DcMotor rightMotor;
+
+    public AHATest() {
+    }
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.dcMotor.get("left");
+        rightMotor = hardwareMap.dcMotor.get("right");
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.right_bumper) {
+            rightMotor.setPower(10);
+        } else if(gamepad1.right_stick_button){
+            rightMotor.setPower(-10);
+        } else {
+            leftMotor.setPower(0);
+        }
+
+        if(gamepad1.left_bumper) {
+            leftMotor.setPower(10);
+        } else if(gamepad1.left_stick_button){
+            leftMotor.setPower(-10);
+        } else {
+            leftMotor.setPower(0);
+        }
+    }
+}
