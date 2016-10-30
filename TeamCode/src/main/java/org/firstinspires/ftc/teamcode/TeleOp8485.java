31d6b8fa907a2d85355957ee996f71be45bf02ee
Devon <dpulrich@gmail.com>
2016-10-29 22:02:40 -0400

    Added basic movement to robot
    We added control to the four motors for the robot wheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp8485.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp8485.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by harry on 2016-10-29.
@@ -10,13 +12,35 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="StandardTeleOp", group="8485")
 public class TeleOp8485 extends OpMode {
 
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+
     @Override
     public void init() {
+        frontLeft = hardwareMap.dcMotor.get("front_l");
+        frontRight = hardwareMap.dcMotor.get("front_r");
+        backLeft = hardwareMap.dcMotor.get("back_l");
+        backRight = hardwareMap.dcMotor.get("back_r");
 
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
     }
 
     @Override
     public void loop() {
+        double left = -gamepad1.left_stick_y;
+        double right = -gamepad1.right_stick_y;
 
+        frontLeft.setPower(left);
+        backLeft.setPower(left);
+        frontRight.setPower(right);
+        backRight.setPower(right);
     }
 }
