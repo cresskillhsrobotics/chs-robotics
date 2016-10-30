ffa453999febcbc8c5bfe60df5895d061c91376b
Devon <dpulrich@gmail.com>
2016-10-29 22:55:52 -0400

    Added all other motors
    We added control of the other motors, such as the zip tie wheel
    and the tank tread. However, the values and directions might need
    to change, depending on how the robot runs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp8485.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp8485.java
@@ -17,6 +17,11 @@ public class TeleOp8485 extends OpMode {
     DcMotor backLeft;
     DcMotor backRight;
 
+    DcMotor zipTie;
+    DcMotor tankTread;
+
+    DcMotor shooter;
+
     @Override
     public void init() {
         frontLeft = hardwareMap.dcMotor.get("front_l");
@@ -24,13 +29,25 @@ public class TeleOp8485 extends OpMode {
         backLeft = hardwareMap.dcMotor.get("back_l");
         backRight = hardwareMap.dcMotor.get("back_r");
 
+        zipTie = hardwareMap.dcMotor.get("zip_tie");
+        tankTread = hardwareMap.dcMotor.get("tread");
+
+        shooter = hardwareMap.dcMotor.get("shoot");
+
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        //zipTie.setDirection(DcMotor.Direction.REVERSE);
+        //tankTread.setDirection(DcMotor.Direction.REVERSE);
+        //shooter.setDirection(DcMotor.Direction.REVERSE);
 
         frontLeft.setPower(0);
         frontRight.setPower(0);
         backLeft.setPower(0);
         backRight.setPower(0);
+        zipTie.setPower(0);
+        tankTread.setPower(0);
+        shooter.setPower(0);
     }
 
     @Override
@@ -42,5 +59,29 @@ public class TeleOp8485 extends OpMode {
         backLeft.setPower(left);
         frontRight.setPower(right);
         backRight.setPower(right);
+
+        if(gamepad1.a) {
+            zipTie.setPower(0.5);
+        } else if(gamepad1.y) {
+            zipTie.setPower(-0.5);
+        } else {
+            zipTie.setPower(0);
+        }
+
+        if(gamepad1.right_bumper) {
+            tankTread.setPower(0.5);
+        } else if(gamepad1.right_trigger != 0) {
+            tankTread.setPower(-0.5);
+        } else {
+            tankTread.setPower(0);
+        }
+
+        if(gamepad1.left_bumper) {
+            shooter.setPower(1);
+        } else if(gamepad1.right_trigger != 0) {
+            shooter.setPower(-1);
+        } else {
+            shooter.setPower(0);
+        }
     }
 }
