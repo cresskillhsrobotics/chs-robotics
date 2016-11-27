83800d932e01d352cc5acb2b763dc75bf8d5a1aa
Jacob Gorman <violinoman18@aol.com>
2016-11-26 19:37:06 -0500

    Renamed motors and Removed old code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp6983.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp6983.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
@@ -13,13 +12,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name="Teleop6983", group="6983")
 public class TeleOp6983 extends OpMode {
 
-    DcMotor fl;
-    DcMotor fr;
-    DcMotor bl;
-    DcMotor br;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
 
-    DcMotor m1;
-    DcMotor m2;
+    DcMotor rightLauncher;
+    DcMotor leftLauncher;
 
     Servo s1;
     Servo s2;
@@ -31,75 +30,44 @@ public class TeleOp6983 extends OpMode {
 
     @Override
     public void init() {
-        fl = hardwareMap.dcMotor.get("front_l");
-        fr = hardwareMap.dcMotor.get("front_r");
-        bl = hardwareMap.dcMotor.get("back_l");
-        br = hardwareMap.dcMotor.get("back_r");
+        frontLeft = hardwareMap.dcMotor.get("front_left");
+        frontRight = hardwareMap.dcMotor.get("front_right");
+        backLeft = hardwareMap.dcMotor.get("back_left");
+        backRight = hardwareMap.dcMotor.get("back_right");
 
-        m1 = hardwareMap.dcMotor.get("motor_one");
-        m2 = hardwareMap.dcMotor.get("motor_two");
+
+        rightLauncher = hardwareMap.dcMotor.get("launch_right");
+        leftLauncher = hardwareMap.dcMotor.get("launch_left");
 
         s1 = hardwareMap.servo.get("servo_one");
         s2 = hardwareMap.servo.get("servo_two");
 
-        fl.setPower(0);
-        fr.setPower(0);
-        bl.setPower(0);
-        br.setPower(0);
-        m1.setPower(0);
-        m2.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
 
-        s1.setPosition(0);
-        s2.setPosition(0);
+        rightLauncher.setPower(0);
+        leftLauncher.setPower(0);
     }
 
     @Override
     public void loop() {
-
         if(gamepad1.left_stick_y > 0.25 || gamepad1.left_stick_x > 0.25 || gamepad1.left_stick_y < -0.25 || gamepad1.left_stick_x < -0.25) {
-            fr.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
-            fl.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
-            br.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
-            bl.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
+            frontRight.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
+            frontLeft.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
+            backRight.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
+            backLeft.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x)/2);
         } else if(gamepad1.right_stick_y > 0.25 || gamepad1.right_stick_y < -0.25) {
-            fr.setPower((gamepad1.right_stick_y)/2);
-            fl.setPower(-(gamepad1.right_stick_y)/2);
-            br.setPower((gamepad1.right_stick_y)/2);
-            bl.setPower(-(gamepad1.right_stick_y)/2);
+            frontRight.setPower((gamepad1.right_stick_y)/2);
+            frontLeft.setPower(-(gamepad1.right_stick_y)/2);
+            backRight.setPower((gamepad1.right_stick_y)/2);
+            backLeft.setPower(-(gamepad1.right_stick_y)/2);
         } else {
-            fr.setPower(0);
-            fl.setPower(0);
-            br.setPower(0);
-            bl.setPower(0);
+            frontRight.setPower(0);
+            frontLeft.setPower(0);
+            backRight.setPower(0);
+            backLeft.setPower(0);
         }
-
-        if (gamepad2.x)
-            s1Position += S1_SPEED;
-        else if (gamepad2.y)
-            s1Position -= S1_SPEED;
-
-        if (gamepad2.a)
-            s2Position += S2_SPEED;
-        else if (gamepad2.b)
-            s2Position -= S2_SPEED;
-
-        if(gamepad1.right_bumper) {
-            m1.setPower(0.5);
-        } else if(gamepad1.left_bumper) {
-            m1.setPower(-0.5);
-        } else {
-            m1.setPower(0);
-        }
-
-        if(gamepad2.right_bumper) {
-            m2.setPower(0.5);
-        } else if(gamepad2.left_bumper) {
-            m2.setPower(-0.5);
-        } else {
-            m2.setPower(0);
-        }
-
-        s1.setPosition(s1Position);
-        s2.setPosition(s2Position);
     }
 }
